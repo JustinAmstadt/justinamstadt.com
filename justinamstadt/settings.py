0a0f998e6fc5de31b2222b2b458ef1c46f634e58
JustinAmstadt <justinamstadt@gmail.com>
2025-12-10 16:43:07 -0800

    add browser hot reloading for templates

--- justinamstadt/settings.py
+++ justinamstadt/settings.py
@@ -53,6 +53,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+if DEBUG:
+    INSTALLED_APPS.append("django_browser_reload")
+    MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
+
 ROOT_URLCONF = "justinamstadt.urls"
 
 TEMPLATES = [
--- justinamstadt/urls.py
+++ justinamstadt/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("", include("general_site.urls")),
     path("zk/", include("zk.urls")),
     path("percolation/", include("percolation.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
